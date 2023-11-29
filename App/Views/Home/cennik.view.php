895fddb51f6948e7dda631750b97de9f92e73aec
NinaDusova <ninadusova7@gmail.com>
2023-11-30 00:56:02 +0100

    Prepojenia na stránky Rezervácie, Cenník, Kontakty

---
+++ App/Views/Home/cennik.view.php
@@ -0,0 +1,125 @@
+<!--price list -------------------------------------------------->
+<div class="price-section">
+    <div class="price-list">
+        <div class="rectangle">
+            <h4>Plague</h4>
+            <div class="small-rectangle">
+                <img src="/public/images/cennik/plague.jpg" alt="">
+            </div>
+            <div class="small-rectangle">
+                <h2 class="price-text">
+                    Vyriešte záhadu choroby ktorá zapríčinila zhubu po celom svete
+                </h2>
+                <h2 class="price-text">
+                    Skupina (2 hráči) - 55€
+                </h2>
+                <h2 class="price-text">
+                    Skupina (3 - 5 hráčov) - 65€
+                </h2>
+                <h2 class="price-text">
+                    Študentská skupinová zľava s ISIC 4€
+                </h2>
+
+                <a href="#" class="icon-link">
+                    Zisti viac o hre
+                    <svg class="bi" width="1em" height="1em"><use xlink:href="#chevron-right"></use></svg>
+                </a>
+            </div>
+        </div>
+
+        <div class="rectangle">
+            <h4>Cake</h4>
+            <div class="small-rectangle">
+                <img src="/public/images/cennik/cake.jpg" alt="">
+            </div>
+            <div class="small-rectangle">
+                <h2 class="price-text">
+                    Koláč ,ktorý otrávil celú rodinu ?!
+                </h2>
+                <h2 class="price-text">
+                    Skupina (2 hráči) - 60€
+                </h2>
+                <h2 class="price-text">
+                    Skupina (3 hráčov) - 70€
+                </h2>
+                <h2 class="price-text">
+                    Skupina (4 - 5 hráčov) - 80€
+                </h2>
+                <h2 class="price-text">
+                    Študentská skupinová zľava s ISIC 4€
+                </h2>
+
+                <a href="#" class="icon-link">
+                    Zisti viac o hre
+                    <svg class="bi" width="1em" height="1em"><use xlink:href="#chevron-right"></use></svg>
+                </a>
+            </div>
+        </div>
+        <div class="rectangle">
+            <h4>Hello Kitty murder</h4>
+            <div class="small-rectangle">
+                <img src="/public/images/cennik/hello-kitty.png" alt="">
+            </div>
+            <div class="small-rectangle">
+                <h2 class="price-text">
+                    Máš na to aby si vyriešil tento prípad detektív?
+                </h2>
+                <h2 class="price-text">
+                    Skupina (2 hráči) - 65€
+                </h2>
+                <h2 class="price-text">
+                    Skupina (3 - 5 hráčov) - 75€
+                </h2>
+                <h2 class="price-text">
+                    Študentská skupinová zľava s ISIC 4€
+                </h2>
+
+                <a href="#" class="icon-link">
+                    Zisti viac o hre
+                    <svg class="bi" width="1em" height="1em"><use xlink:href="#chevron-right"></use></svg>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--price list -------------------------------------------------->
+
+
+<!--bottom -------------------------------------------------->
+<div class="bottom">
+    <div class="long-rectangle">
+        <div class="small-rectangle">
+            <a class="navbar-brand" href="#">
+                <img src="/public/images/logo.webp" class="logo" alt="Bootstrap">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <ul class="navbar-nav mb-2 mb-sm-0">
+                <li class="nav-item">
+                    <a class="nav-link active bottom-link" aria-current="page" href="#"><i class=""><i class="bi bi-telephone"></i></i> +421 955 453 521</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav mb-2 mb-sm-0">
+                <li class="nav-item">
+                    <a class="nav-link active bottom-link" aria-current="page" href="#"><i class=""><i class="bi bi-envelope-at"></i></i> pink.room.info@gmail.com</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav mb-2 mb-sm-0">
+                <li class="nav-item">
+                    <a class="nav-link active bottom-link" aria-current="page" href="#"><i class=""><i class="bi bi-geo-alt"></i></i> PinkRoom, Null, BlackHole</a>
+                </li>
+            </ul>
+        </div>
+        <div class="small-rectangle"></div>
+        <div class="small-rectangle">
+            <!-- map ----------------------------------------------------->
+            <!-- <div id="map" style="width: 300px; height: 270px">
+                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18906.129712753736!2d6.722624160288201!3d60.12672284414915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x463e997b1b6fc09d%3A0x6ee05405ec78a692!2sJ%C4%99zyk%20trola!5e0!3m2!1spl!2spl!4v1672239918130!5m2!1spl!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+             </div> -->
+            <!-- map ----------------------------------------------------->
+        </div>
+    </div>
+</div>
+<!--bottom -------------------------------------------------->
---
+++ App/Views/Home/reservation.view.php
@@ -0,0 +1,120 @@
+<?php
+?>
+
+<div class="center container">
+    <table class="table table-date">
+        <thead class="thead-date">
+        <tr>
+            <th scope="col">Po</th>
+            <th scope="col">Ut</th>
+            <th scope="col">St</th>
+            <th scope="col">Št</th>
+            <th scope="col">Pia</th>
+            <th scope="col">So</th>
+            <th scope="col">Ne</th>
+        </tr>
+        </thead>
+
+        <tbody>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="day">1</td>
+        </tr>
+
+        <tr>
+            <td class="day">2</td>
+            <td class="day">3</td>
+            <td class="day">4</td>
+            <td class="day">5</td>
+            <td class="day">6</td>
+            <td class="day">7</td>
+            <td class="day">8</td>
+        </tr>
+
+        <tr>
+            <td class="day">9</td>
+            <td class="day">10</td>
+            <td class="day">11</td>
+            <td class="day">12</td>
+            <td class="day">13</td>
+            <td class="day">14</td>
+            <td class="day">15</td>
+        </tr>
+
+        <tr>
+            <td class="day">16</td>
+            <td class="day">17</td>
+            <td class="day">18</td>
+            <td class="day">19</td>
+            <td class="day">20</td>
+            <td class="day">21</td>
+            <td class="day">22</td>
+        </tr>
+
+        <tr>
+            <td class="day">23</td>
+            <td class="day">24</td>
+            <td class="day">25</td>
+            <td class="day">26</td>
+            <td class="day">27</td>
+            <td class="day">28</td>
+            <td class="day">29</td>
+        </tr>
+
+        <tr>
+            <td class="day">30</td>
+            <td class="day">31</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        </tbody>
+    </table>
+
+    <div class="month">
+        <h3 class="month-name">← Október 2023 →</h3>
+    </div>
+
+    <table class="table checkbox table-checkbox">
+        <tbody>
+        <tr>
+            <td>13:00</td>
+            <td>14:00</td>
+            <td>15:00</td>
+        </tr>
+
+        <tr>
+            <td>16:00</td>
+            <td>17:00</td>
+            <td>18:00</td>
+        </tr>
+
+        <tr>
+            <td>19:00</td>
+            <td>20:00</td>
+            <td></td>
+        </tr>
+        </tbody>
+    </table>
+
+    <div class="contacts">
+        <label for="box-name">Meno a priezvisko: </label>
+        <input type="text" id="box-name" class="box" placeholder="Meno a priezvisko">
+
+        <label for="box-phone">Telefónne číslo: </label>
+        <input type="text" id="box-phone" class="box" placeholder="Telefónne číslo">
+
+        <label for="box-email">Email: </label>
+        <input type="text" id="box-email" class="box" placeholder="Email">
+
+        <h2 style="text-align: center">Osobné údaje sú nevyhnutné pre vytvorenie rezervácie</h2>
+    </div>
+</div>
